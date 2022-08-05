9d9a0f4a1ad4e9d243ff7d821e897fbd824d103e
risa nussy <risanussy07@gmail.com>
2022-08-05 11:18:05 +0700

    20% kiri

--- src/pages/PengelolaSampah.js
+++ src/pages/PengelolaSampah.js
@@ -12,6 +12,12 @@ const PengelolaSampah = () => {
                 <button className="btn-jumb">Kirim Sampah</button>
                 </Link>
             </div>
+            <br /><br /><br /><br />
+            <center>
+                <h3 className='title-text-lp'>Cara Mengirim Sampah</h3>
+                <span className='linegreen'></span>
+            </center>
+            <br /><br />
         </div>
     )
 }
